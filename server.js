4682d8609939279daf73849d7334c424183fc70b
1218hiyokoW2 <192060988+1218hiyokoW2@users.noreply.github.com>
2025-11-14 11:51:32 +0900

    リソースの作成

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ const kv = await Deno.openKv();
 
 /***  リソースの作成 ***/
 app.post('/api/pokemons', async (c) => {
-  return c.json({ path: c.req.path });
+  // リクエストボディを取得
+  const body = await c.req.parseBody();
+  const record = JSON.parse(body['record']);
+
+  // IDと生成時刻を生成してレコードに追加
+  const id = await getNextId();
+  record.id = id;
+  record.createdAt = new Date().toISOString();
+
+  // リソースの作成
+  await kv.set(['pokemons', id], record);
+
+  // レスポンスの作成
+  c.status(201); // 201 Created
+  c.header('Location', `/api/pokemons/${id}`);
+
+  return c.json({ record });
 });
 
 /*** リソースの取得（レコード単体） ***/
